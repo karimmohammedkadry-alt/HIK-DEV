82dc629777d34453d25b2231026673a20fa7f0b4
karimmohammedkadry-alt <karimmohammedkadry@gmail.com>
2026-07-21 19:05:24 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,3 +10,37 @@ def home():
 if __name__ == '__main__':
     # لتشغيل السيرفر المحلي لديك
     app.run(debug=True, port=5000)
+    
+    
+    
+    from datetime import datetime
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import gspread
+
+app = Flask(__name__)
+CORS(app)  # عشان نسمح لموقعك يبعت بيانات للسيرفر من غير مشاكل أمان
+
+# الاتصال بجوجل شيت
+gc = gspread.service_account(filename="credentials.json")
+sh = gc.open("HIK Store Order")  # اسم جوجل شيت بتاعك
+worksheet = sh.sheet1
+
+
+@app.route("/order", methods=["POST"])
+def receive_order():
+  data = request.json
+  name = data.get("name")
+  phone = data.get("phone")
+  product = data.get("product")
+  current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+  # تسجيل الأوردر في جوجل شيت تلقائياً
+  worksheet.append_row([name, phone, product, current_time])
+
+  return jsonify({"status": "success", "message": "تم تسجيل الأوردر!"})
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
+
